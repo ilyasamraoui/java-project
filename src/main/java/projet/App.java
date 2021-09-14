cf11d29857dab9beaa2a9f44be69952ae9ca58a7
ilyasamraoui <52576012+ilyasamraoui@users.noreply.github.com>
2021-09-14 15:35:46 +0100

    Update App.java

--- src/main/java/projet/App.java
+++ src/main/java/projet/App.java
@@ -2,6 +2,6 @@ package projet;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hey man you're alright.. just .. keep it up.");
+        System.out.println("hey man you're alright.. just.. keep it up :)");
     }
 }
